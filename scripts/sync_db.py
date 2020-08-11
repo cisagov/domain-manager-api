b17fd5d62ea100b404bb1609e4465262e62d0481
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-08-11 10:46:25 -0500

    add namecheap to sync script

--- scripts/sync_db.py
+++ scripts/sync_db.py
@@ -6,6 +6,7 @@ import os
 # Third-Party Libraries
 import boto3
 from dotenv import load_dotenv
+from namecheap import Api
 
 # cisagov Libraries
 import pymongo
@@ -21,6 +22,15 @@ CONN_STR = "mongodb://{}:{}@localhost:27016/".format(
 client = pymongo.MongoClient(CONN_STR)
 
 db = client.domain_management
+
+# Initialize namecheap api client
+nc_api = Api(
+    ApiUser=os.environ.get("NC_USERNAME"),
+    UserName=os.environ.get("NC_USERNAME"),
+    ApiKey=os.environ.get("NC_API_KEY"),
+    ClientIP=os.environ.get("NC_IP"),
+    sandbox=False,
+)
 
 
 def load_s3():
@@ -48,4 +58,17 @@ def load_s3():
         logger.info("Database has been synchronized with S3 data.")
 
 
-load_s3()
+def load_domains():
+    """Load the latest domain data from namecheap into the database."""
+    db_domains = db.domains
+    domain_load = [i for i in nc_api.domains_getList()]
+
+    # Save latest data to the database
+    if domain_load != []:
+        db_domains.insert_many(domain_load)
+        logger.info("Database has been synchronized with domain data.")
+
+
+if __name__ == "__main__":
+    load_domains()
+    load_s3()
