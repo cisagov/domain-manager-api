c5cbf9a05aa8932697363e6f7f595c684da966c8
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-09-14 13:03:54 -0700

    update controller and schema

--- src/api/controllers/active_sites.py
+++ src/api/controllers/active_sites.py
@@ -4,7 +4,7 @@ from api.documents.active_site import ActiveSite
 from api.documents.domain import Domain
 from api.documents.website import Website
 from api.schemas.active_site_schema import ActiveSiteSchema
-from utils.aws import delete_site, launch_site
+from utils.aws import delete_site, launch_site, setup_dns, delete_dns
 
 
 def active_site_manager(request, live_site_id=None):
@@ -12,21 +12,35 @@ def active_site_manager(request, live_site_id=None):
     if not live_site_id:
         if request.method == "POST":
             post_data = request.json
+            domain = Domain.get_by_id(post_data.get("domain_id"))
             application_id = post_data.get("application_id")
             website = Website.get_by_id(post_data.get("website_id"))
-            domain = Domain.get_by_id(post_data.get("domain_id"))
-            # launch s3 bucket and set dns
-            live_site = launch_site(website, domain)
-            # save to database
-            active_site = ActiveSite.create(
-                description=post_data.get("description"),
-                s3_url=live_site,
-                domain_id=post_data.get("domain_id"),
-                website_id=post_data.get("website_id"),
-                application_id=application_id,
-            )
+            ip_address = post_data.get("ip_address")
+
+            if ip_address:
+                # set dns
+                setup_dns(domain=domain, ip_address=ip_address)
+                # save to database
+                active_site = ActiveSite.create(
+                    description=post_data.get("description"),
+                    domain_id=post_data.get("domain_id"),
+                    ip_address=ip_address,
+                    application_id=application_id,
+                )
+            else:
+                # launch s3 bucket and set dns
+                live_site = launch_site(website, domain)
+                # save to database
+                active_site = ActiveSite.create(
+                    description=post_data.get("description"),
+                    s3_url=live_site,
+                    domain_id=post_data.get("domain_id"),
+                    website_id=post_data.get("website_id"),
+                    application_id=application_id,
+                )
+            domain_name = domain.get("Name")[:-1]
             response = {
-                "message": f"Active site with id {active_site.inserted_id} has been launched. Visit: http://{live_site}"
+                "message": f"Active site with id {active_site.inserted_id} has been launched. Visit: http://{domain_name}"
             }
         else:
             active_sites_schema = ActiveSiteSchema(many=True)
@@ -36,8 +50,13 @@ def active_site_manager(request, live_site_id=None):
     if request.method == "DELETE":
         active_site = ActiveSite.get_by_id(live_site_id)
         domain = Domain.get_by_id(active_site.get("domain").get("_id"))
-        # delete s3 bucket and remove dns from domain
-        delete_site(domain)
+        ip_address = active_site.get("ip_address")
+        if ip_address:
+            # delete A record from dns
+            delete_dns(domain=domain, ip_address=ip_address)
+        else:
+            # delete s3 bucket and remove dns from domain
+            delete_site(domain)
         # delete from database
         ActiveSite.delete(live_site_id)
         response = {"message": "Active site is now inactive and deleted."}
