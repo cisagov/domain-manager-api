9c6ffd664e047bb438c881f3698a561f256d56be
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-11-17 12:32:15 -0600

    s3 bucket update

--- terraform/api.tf
+++ terraform/api.tf
@@ -69,6 +69,30 @@ POLICY
   }
 }
 
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.app}-${var.env}-bucket"
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Public",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.app}-${var.env}-bucket/*"
+    }
+  ]
+}
+POLICY
+
+  website {
+    index_document = "index.html"
+  }
+}
+
 resource "aws_s3_bucket" "templates" {
   bucket = "${var.app}-${var.env}-templates"
   acl    = "private"
