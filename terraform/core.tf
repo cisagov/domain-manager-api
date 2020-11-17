6d8457d3fc97c4f80cac339cada599c1caa752e9
Zachary Wilson <wilsonzachary634@gmail.com>
2020-11-17 07:14:42 -0700

    acm validation fix

--- terraform/core.tf
+++ terraform/core.tf
@@ -25,19 +25,19 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "validation" {
-  allow_overwrite = true
-  name            = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  records         = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
-  ttl             = 60
-  type            = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  zone_id         = data.aws_route53_zone.zone.zone_id
-}
+# resource "aws_route53_record" "validation" {
+#   allow_overwrite = true
+#   name            = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+#   records         = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+#   ttl             = 60
+#   type            = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+#   zone_id         = data.aws_route53_zone.zone.zone_id
+# }
 
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.validation.fqdn]
-}
+# resource "aws_acm_certificate_validation" "validation" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [aws_route53_record.validation.fqdn]
+# }
 
 
 # ===================================
