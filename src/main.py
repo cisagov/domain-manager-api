48fd2724daa1eeb672ebb12542861879a4ff0983
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-11-20 12:30:56 -0600

    remove swagger, replace with simple api map

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import os
 
 # Third-Party Libraries
 from api import api
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,9 +15,14 @@ app.register_blueprint(api)
 
 
 @app.route("/")
-def get_docs():
-    """Access swagger UI."""
-    return render_template("swagger.html")
+def api_map():
+    """API map."""
+    endpoints = {
+        endpoint.rule: endpoint.methods
+        for endpoint in app.url_map.__dict__["_rules"]
+        if endpoint.rule not in ["/static/<path:filename>", "/"]
+    }
+    return render_template("index.html", endpoints=endpoints)
 
 
 if __name__ == "__main__":
