fe7cf6fc617add76ddc4b47bb2904ae630c1e9c2
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-12-02 11:32:00 -0600

    remove dup env var

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 
 # aws
-AWS_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
+AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 TEMPLATE_BUCKET = os.environ["TEMPLATE_BUCKET"]
 TEMPLATE_BUCKET_URL = f"http://{TEMPLATE_BUCKET}.s3-website-{AWS_REGION}.amazonaws.com/"
 
