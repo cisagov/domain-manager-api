666283c3592d0b4bbdf8138d32798accd205d24a
Zachary Wilson <wilsonzachary634@gmail.com>
2020-12-02 07:50:33 -0700

    move terraform to cicd repo

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "inl-tf-backend"
-    key            = "domain-manager-api"
-    region         = "us-east-1"
-    dynamodb_table = "inl-tf-lock"
-  }
-}
