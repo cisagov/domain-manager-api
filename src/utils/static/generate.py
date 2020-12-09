2eb58dd5b6e3ed15d6155512d5853051e6bb15a5
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-12-08 22:24:22 -0600

    update python calls to staticgen

--- src/utils/static/generate.py
+++ src/utils/static/generate.py
@@ -4,10 +4,29 @@ import requests
 from settings import STATIC_GEN_URL
 
 
+def upload_template(category):
+    """Upload template files."""
+    resp = requests.post(f"{STATIC_GEN_URL}/template/?category={category}")
+    return {"message": resp.status_code}
+
+
+def delete_template(category):
+    """Delete template files."""
+    resp = requests.delete(f"{STATIC_GEN_URL}/template/?category={category}")
+    return {"message": resp.status_code}
+
+
 def generate_site(category, domain):
     """Generate a static site."""
-    resp = requests.get(
-        f"{STATIC_GEN_URL}/website/?category={category}&domain={domain}"
+    post_data = {
+        "description": "This is from a POST request",
+        "domain": "www.spokanepestservices.com",
+        "email": "spokane@mypestcompany.com",
+        "name": "My Spokane Pest Services FROM POST",
+        "phone": "661-456-7890",
+    }
+    resp = requests.post(
+        f"{STATIC_GEN_URL}/website/?category={category}&domain={domain}", json=post_data
     )
     return {"message": resp.status_code}
 
