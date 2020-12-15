5a44178aae2ad00df0ce8fe8b98c1611bb683400
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-12-14 20:11:10 -0600

    add generate endpoint

--- src/staticgen/controllers.go
+++ src/staticgen/controllers.go
@@ -12,8 +12,8 @@ import (
 // Set S3 bucket URL
 var bucket = os.Getenv("TEMPLATE_BUCKET")
 
-// WebsiteHandler generates static websites from templates
-func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
+// GenerateHandler generates website files from template files in s3
+func GenerateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	query := r.URL.Query()
@@ -26,13 +26,6 @@ func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 		decoder.Decode(&context)
 		route.Generate(&context)
-	} else if r.Method == "GET" {
-		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", "Website"))
-		route.Download(w)
-	} else if r.Method == "DELETE" {
-		route.Delete()
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
 
@@ -52,6 +45,33 @@ func TemplateHandler(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == "GET" {
 		w.Header().Set("Content-Type", "application/zip")
 		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", category))
+		route.Download(w)
+	} else if r.Method == "DELETE" {
+		route.Delete()
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+// WebsiteHandler generates static websites from templates
+func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	query := r.URL.Query()
+	category := query.Get("category")
+	domain := query.Get("domain")
+
+	route := aws.Route{Bucket: bucket, Category: category, Dir: domain}
+	if r.Method == "POST" {
+		// Recieve and unzip file
+		foldername, err := Receive(r, category)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Upload to S3
+		route.Upload(foldername)
+	} else if r.Method == "GET" {
+		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", "Website"))
 		route.Download(w)
 	} else if r.Method == "DELETE" {
 		route.Delete()
