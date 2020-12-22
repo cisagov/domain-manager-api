d2b064f8ae5b04c4b8b5e799ba11d9e36b863c9e
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-12-22 00:02:03 -0600

    update views

--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,9 @@
 """API routes."""
+# Standard Python Libraries
+import io
+
 # Third-Party Libraries
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, send_file
 
 # cisagov Libraries
 from api.controllers.applications import applications_manager
@@ -9,6 +12,7 @@ from api.controllers.check import check_categories_manager
 from api.controllers.email_address import email_address_manager
 from api.controllers.hosted_zones import hosted_zones_manager
 from api.controllers.proxies import proxy_manager
+from api.controllers.templates import template_manager
 from api.controllers.websites import generate_website_manager, website_manager
 from utils.decorators.auth import auth_required
 
@@ -37,42 +41,42 @@ def get_application(application_id):
 @auth_required
 def get_categories():
     """Check all categories."""
-    response = categories_manager()
-    return jsonify(response)
+    resp = categories_manager()
+    return jsonify(resp)
 
 
 @api.route("/categorize/<website_id>/", methods=["GET"])
 @auth_required
 def categorize_domain(website_id):
     """Categorize an active site by using available proxies."""
-    response = categorization_manager(
+    resp = categorization_manager(
         website_id=website_id, category=request.args.get("category")
     )
-    return jsonify(response)
+    return jsonify(resp)
 
 
 @api.route("/check/", methods=["GET"])
 @auth_required
 def check_domain():
     """Check domain categorization."""
-    response = check_categories_manager(request.args.get("domain"))
-    return jsonify(response)
+    resp = check_categories_manager(request.args.get("domain"))
+    return jsonify(resp)
 
 
 @api.route("/generate-dns/", methods=["POST", "DELETE"])
 @auth_required
 def generate_dns_record_handler():
     """Generate DNS record handlers in AWS Route53."""
-    response = hosted_zones_manager(request)
-    return jsonify(response)
+    resp = hosted_zones_manager(request)
+    return jsonify(resp)
 
 
 @api.route("/generate-email-address/")
 @auth_required
 def generate_email_address():
     """Generate an email address using AWS SES."""
-    response = email_address_manager(request.args.get("domain"))
-    return jsonify(response)
+    resp = email_address_manager(request.args.get("domain"))
+    return jsonify(resp)
 
 
 @api.route("/proxies/", methods=["GET", "POST"])
@@ -93,6 +97,33 @@ def get_proxy(proxy_id):
     return jsonify(proxy_manager(request, proxy_id=proxy_id)), 200
 
 
+@api.route("/templates/", methods=["GET", "POST"])
+@auth_required
+def templates_list():
+    """Get a list of templates."""
+    resp = template_manager()
+    return jsonify(resp)
+
+
+@api.route("/template/<template_id>/", methods=["GET"])
+@auth_required
+def template(template_id):
+    """Download template zipfile."""
+    resp, category = template_manager(template_id=template_id)
+
+    # Create buffer
+    buffer = io.BytesIO()
+    buffer.write(resp)
+    buffer.seek(0)
+
+    return send_file(
+        buffer,
+        as_attachment=True,
+        attachment_filename=f"{category}.zip",
+        mimetype="application/zip",
+    )
+
+
 @api.route("/websites/", methods=["GET"])
 @auth_required
 def website_list():
@@ -104,7 +135,21 @@ def website_list():
 @auth_required
 def website(website_id):
     """Manage a website by its id."""
-    return jsonify(website_manager(request, website_id=website_id)), 200
+    resp, domain = website_manager(request, website_id=website_id)
+    if request.method == "GET":
+        # Create buffer
+        buffer = io.BytesIO()
+        buffer.write(resp)
+        buffer.seek(0)
+
+        return send_file(
+            buffer,
+            as_attachment=True,
+            attachment_filename=f"{domain}.zip",
+            mimetype="application/zip",
+        )
+
+    return jsonify(resp), 200
 
 
 @api.route("website/<website_id>/generate/", methods=["POST"])
@@ -112,5 +157,5 @@ def website(website_id):
 def generate_website(website_id):
     """Generate website files from a template."""
     category = request.args.get("category")
-    response = generate_website_manager(request, website_id, category)
-    return jsonify(response)
+    resp = generate_website_manager(request, website_id, category)
+    return jsonify(resp)
