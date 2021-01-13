73cd9230bdec4eedc5aeb1f1ee681e6f3e183439
Zachary Wilson <wilsonzachary634@gmail.com>
2021-01-13 15:24:49 -0700

    tests init

---
+++ src/tests/data/application_data.py
@@ -0,0 +1,24 @@
+"""Application Fake Data."""
+# Third-Party Libraries
+from faker import Faker
+
+fake = Faker()
+
+
+def get_application():
+    """Get single app."""
+    return {
+        "_id": fake.uuid4(),
+        "name": fake.name(),
+        "requester_name": fake.name(),
+        "contact_name": fake.name(),
+        "contact_email": fake.email(),
+        "contact_phone": fake.phone_number(),
+        "created": fake.date_time(),
+        "updated": fake.date_time(),
+    }
+
+
+def get_applications(num=10):
+    """Get multiple apps."""
+    return [get_application() for i in range(0, num)]
---
+++ src/tests/test_flask.py
@@ -0,0 +1,20 @@
+"""Main fixture for testing flask app."""
+# Third-Party Libraries
+import pytest
+
+# cisagov Libraries
+from main import app
+
+
+@pytest.fixture(scope="session")
+def test_client():
+    """Get fixture for client."""
+    app.config["TESTING"] = True
+    app.config["BCRYPT_LOG_ROUNDS"] = 4
+    app.config["WTF_CSRF_ENABLED"] = False
+    testing_client = app.test_client()
+    ctx = app.app_context()
+    ctx.push()
+
+    yield testing_client
+    ctx.pop()
