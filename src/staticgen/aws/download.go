973164f3d66c5a0f21c847e3b1176deb4dc11f89
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-01-20 14:24:22 -0600

    website download fix

--- src/staticgen/aws/download.go
+++ src/staticgen/aws/download.go
@@ -35,7 +35,11 @@ func (fw FakeWriterAt) WriteAt(p []byte, offset int64) (n int, err error) {
 // BufferDownload downloads from s3 bucket to buffer
 func (r *Route) BufferDownload(w http.ResponseWriter, bucket string) {
 	manager := s3manager.NewDownloader(session.New())
-	dir := filepath.Join(r.Dir, "template")
+
+	dir := filepath.Join(r.Dir)
+	if bucket == TemplateBucket {
+		dir = filepath.Join(r.Dir, "template")
+	}
 	d := Downloader{bucket: bucket, dir: dir, Downloader: manager, writer: w}
 
 	client := s3.New(session.New())
