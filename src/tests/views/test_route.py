0bc1687017a488da1636908a629faa80e05fc826
Zachary Wilson <wilsonzachary634@gmail.com>
2021-02-02 09:27:32 -0700

    fix pytest for base route

--- src/tests/views/test_route.py
+++ src/tests/views/test_route.py
@@ -1,7 +1,14 @@
 """Application View Tests."""
 
 
+class SampleResponse:
+    """SampleResponse for requests."""
+
+    text = "test"
+
+
 def test_route_get(client, mocker):
     """Test getting list of applications."""
+    mocker.patch("requests.get", return_value=SampleResponse())
     resp = client.get("/")
     assert resp.status_code == 200
