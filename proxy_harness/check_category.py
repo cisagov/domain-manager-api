fd7214fcd751b2a8c8b5fad696ef2b350c395069
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-02-16 11:53:02 -0800

    update bluecoat check category script

--- proxy_harness/check_category.py
+++ proxy_harness/check_category.py
@@ -1,9 +1,16 @@
 """Categorization controller. Mac and linux compatible."""
 # Standard Python Libraries
+import os
 import time
+
+# Third-Party Libraries
+from selenium import webdriver
 
 # cisagov Libraries
 from utils.proxies.proxies import get_check_proxies
+
+# Load environment variables from .env file
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def check(domain_name):
@@ -11,11 +18,21 @@ def check(domain_name):
     # Submit domain to proxy
     for k, v in get_check_proxies().items():
         try:
-            resp = v(domain_name)
+            driver = webdriver.Chrome(
+                executable_path=f"{script_dir}/drivers/chromedriver"
+            )
+        except OSError:
+            driver = webdriver.Chrome(
+                executable_path=f"{script_dir}/drivers/chromedriver.exe"
+            )
+        try:
+            resp = v(driver, domain_name)
             print(f"{k} responsed with {resp}")
             time.sleep(3)
         except Exception as e:
             print(str(e))
+        finally:
+            driver.quit()
 
 
 domain_name = input("Enter a domain name: ")
--- src/utils/proxies/bluecoat.py
+++ src/utils/proxies/bluecoat.py
@@ -1,10 +1,8 @@
 """Blue Coat Proxy Categorization."""
 # Standard Python Libraries
-import re
 import time
 
 # Third-Party Libraries
-import requests
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -53,33 +51,15 @@ def categorize(driver, domain, category, two_captcha_api_key):
     driver.find_element(By.ID, "submit2").click()
 
 
-def check_category(domain):
+def check_category(driver, domain):
     """Check domain category on Bluecoat."""
-    print("[*] Checking category for " + domain)
-    data = {"url": domain, "captcha": ""}
-    headers = {
-        "User-Agent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)",
-        "Origin": "https://sitereview.bluecoat.com",
-        "Referer": "https://sitereview.bluecoat.com",
-        "X-Requested-With": "XMLHttpRequest",
-        "X-XSRF-TOKEN": "aaa",
-        "Content-Type": "application/json; charset=utf-8",
-    }
-    cookies = {"XSRF-TOKEN": "aaa"}
-    response = requests.post(
-        "http://sitereview.bluecoat.com/resource/lookup",
-        headers=headers,
-        json=data,
-        cookies=cookies,
-    )
-    try:
-        resp = response.text
-        cat = re.findall(
-            "<name>(.*?)</name></categorization></categorization>", resp, re.DOTALL
-        )
-        print("\033[1;32m[!] Site categorized as: " + cat[0] + "\033[0;0m")
-        return cat[0]
-    except Exception as e:
-        print("An error occurred")
-        print(e)
-        return None
+    print("Checking Bluecoat proxy")
+    driver.get("https://sitereview.bluecoat.com/#/")
+    driver.set_window_size(1765, 1040)
+    driver.find_element(By.ID, "txtUrl").click()
+    driver.find_element(By.ID, "txtUrl").send_keys(f"http://{domain}")
+    driver.find_element(By.ID, "txtUrl").send_keys(Keys.ENTER)
+    time.sleep(1)
+    category = driver.find_element_by_xpath("//span[@class='clickable-category']")
+
+    return category.text
