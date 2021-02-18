e80fd4753f05954355a151d92a6b3214ccc9d253
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-02-17 17:33:05 -0800

    move load dotenv for multiple use

--- proxy_harness/__init__.py
+++ proxy_harness/__init__.py
@@ -3,5 +3,10 @@
 import os
 import sys
 
+# Third-Party Libraries
+from dotenv import load_dotenv
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.realpath(f"{script_dir}/../src"))
+
+load_dotenv(dotenv_path=f"{script_dir}/../.env")
