7a3732f795cb6c1bd87225aa2e8caa8d205d4005
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-02-17 17:34:28 -0800

    update ciscotalos script

--- src/utils/proxies/ciscotalos.py
+++ src/utils/proxies/ciscotalos.py
@@ -1,31 +1,20 @@
 """Cisco Talos categorization check."""
 # Standard Python Libraries
-import json
-import urllib
+import os
+
+# Third-Party Libraries
+import requests
+
+CISCO_API_KEY = os.environ.get("CISCO_API_KEY")
 
 
-def check_category(driver, domain):
+def check_category(domain):
     """Check domain category on Cisco Talos."""
-    request = urllib.request.Request(
-        "https://talosintelligence.com/sb_api/query_lookup?query=%2Fapi%2Fv2%2Fdetails%2Fdomain%2F&query_entry="
-        + domain
-        + "&offset=0&order=ip+asc"
+    print("Checking Cisco Talos proxy")
+    headers = {"Authorization": f"Bearer {CISCO_API_KEY}"}
+    resp = requests.get(
+        f"https://investigate.api.umbrella.com/domains/categorization/{domain}?showLabels",
+        headers=headers,
     )
-    request.add_header(
-        "User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)"
-    )
-    request.add_header(
-        "Referer",
-        "https://www.talosintelligence.com/reputation_center/lookup?search=" + domain,
-    )
-    response = urllib.request.urlopen(request)
-    try:
-        check_json = json.loads(response.read())
-        categorydict = check_json.get("category")
-        cat = categorydict.get("description")
-        print("\033[1;32m[!] Site categorized as: " + cat + "\033[0;0m")
-        return cat
-    except Exception as e:
-        print("An error occurred")
-        print(e)
-        return None
+
+    return "\n".join(resp.json()[domain]["content_categories"])
