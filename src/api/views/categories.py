7efb4c21995623e942be186f468dd32aa908849c
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-02-28 15:24:04 -0600

    add external check categories

--- src/api/views/categories.py
+++ src/api/views/categories.py
@@ -2,9 +2,12 @@
 # Third-Party Libraries
 from flask import jsonify
 from flask.views import MethodView
+from selenium import webdriver
 
 # cisagov Libraries
 from api.views import CATEGORIES
+from settings import BROWSERLESS_ENDPOINT, logger
+from utils.proxies.proxies import get_check_proxies
 
 
 class CategoriesView(MethodView):
@@ -13,3 +16,33 @@ class CategoriesView(MethodView):
     def get(self):
         """Get all categories."""
         return jsonify(CATEGORIES)
+
+
+class CheckCategoriesView(MethodView):
+    """External Check Categories View."""
+
+    def get(self, domain_name):
+        """Get categories for external categories."""
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+
+        driver = webdriver.Remote(
+            command_executor=f"http://{BROWSERLESS_ENDPOINT}/webdriver",
+            desired_capabilities=chrome_options.to_capabilities(),
+        )
+        driver.set_page_load_timeout(60)
+
+        resp = []
+        for k, v in get_check_proxies().items():
+            try:
+                category = v(
+                    driver,
+                    domain_name,
+                )
+                resp.append({k: category})
+            except Exception as e:
+                driver.quit()
+                logger.exception(e)
+
+        return jsonify(resp)
