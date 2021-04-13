6195beb88c21cc160470e589215e59a244ae9ea8
Dylan Johnson <dylanj1752@gmail.com>
2021-04-13 13:13:55 -0600

    Fix object id reference

--- src/utils/logs.py
+++ src/utils/logs.py
@@ -1,10 +1,10 @@
 """Logging utils."""
 # cisagov Libraries
+
 from api.manager import LogManager
+from bson.objectid import ObjectId
 
 log_manager = LogManager()
-
-
 def cleanup_logs(username):
     """Cleanup log collection."""
     to_keep = log_manager.all(
@@ -13,27 +13,5 @@ def cleanup_logs(username):
         limit=100,  # Only keeping 100 for now. Change when a new number is determined.
         fields=["_id"],
     )
-    all_users_logs = log_manager.all(
-        params={"username": username},
-        fields=["_id"],
-    )
-
-    to_keep_array = []
-    all_user_logs_array = []
-    for i in to_keep:
-        to_keep_array.append(i["_id"])
-    for i in all_users_logs:
-        all_user_logs_array.append(i["_id"])
-    set_a = set(to_keep_array)
-    set_b = set(all_user_logs_array)
-
-    to_remove = set_b - set_a
-
-    ids = []
-    for i in to_remove:
-        log_manager.delete(document_id=i)
-        ids.append(i)
-
-    # This is the better way to do this but requires an update to our manager system
-    # reponse = log_manager.delete(params={'_id': {'$in': ids}})
-    return
+    ids = [ObjectId(x.get("_id")) for x in to_keep]
+    return log_manager.delete(params={"_id": {"$nin": ids}, "username": username})
