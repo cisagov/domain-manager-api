bb0d690ef316e5c0519d27ead1aa253d4021537f
Zachary Wilson <zachary.wilson@inl.gov>
2021-04-20 12:41:59 -0600

    readd coveralls to setup.py

--- setup.py
+++ setup.py
@@ -176,6 +176,13 @@ setup(
         "test": [
             "black",
             "coverage",
+            # coveralls 1.11.0 added a service number for calls from
+            # GitHub Actions. This caused a regression which resulted in a 422
+            # response from the coveralls API with the message:
+            # Unprocessable Entity for url: https://coveralls.io/api/v1/jobs
+            # 1.11.1 fixed this issue, but to ensure expected behavior we'll pin
+            # to never grab the regression version.
+            "coveralls != 1.11.0",
             "flake8",
             "moto",
             "pre-commit",
