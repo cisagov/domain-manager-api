e6897b9d33ceb9f72e8f6c75a0e8723d18341008
Zachary Wilson <zachary.wilson@inl.gov>
2021-05-10 11:30:39 -0600

    switch typing to use base

--- src/utils/apex_records.py
+++ src/utils/apex_records.py
@@ -1,20 +1,14 @@
 """Apex records."""
 # Standard Python Libraries
-from typing import Any, Union
+from functools import partial
 
 
 def contains_apex_record(domain: dict):
     """Check if contains an apex record."""
-    return bool(
-        next(
-            filter(
-                lambda x: is_apex_record(x, domain["name"]), domain.get("records", [])
-            ),
-            False,
-        )
-    )
+    f = partial(is_apex_record, domain["name"])
+    return bool(next(filter(f, domain.get("records", [])), False))
 
 
-def is_apex_record(record: Union[Any, Any], domain_name: str):
+def is_apex_record(domain_name: str, record: dict):
     """Check if it is an apex record."""
     return record["name"] == domain_name and record["record_type"] == "A"
