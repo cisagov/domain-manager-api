fbbd6a5e318085c3e5ac9e36029264a43573b386
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-07-29 13:14:21 -0500

    remove add mx record and ses fixes

--- src/utils/aws/ses.py
+++ src/utils/aws/ses.py
@@ -1,21 +1,9 @@
 """Create DNS email records from SES."""
-# Third-Party Libraries
-import boto3
+# cisagov Libraries
+from utils.aws.clients import SES, Route53
 
-route53 = boto3.client("route53")
-ses = boto3.client("ses")
-
-
-def list_hosted_zones(names_only: bool = False):
-    """
-    List hosted zones.
-
-    Set names_only to true if only hosted zone names are needed.
-    """
-    if not names_only:
-        return route53.list_hosted_zones()["HostedZones"]
-
-    return [hosted_zone.get("Name") for hosted_zone in list_hosted_zones()]
+route53 = Route53()
+ses = SES()
 
 
 def manage_resource_records(
@@ -24,16 +12,16 @@ def manage_resource_records(
     verification_token: str,
 ):
     """Manage Route53 Records."""
-    if f"{domain_name}." not in list_hosted_zones(names_only=True):
+    if f"{domain_name}." not in route53.list_hosted_zones(names_only=True):
         return "Domain's hosted zone does not exist."
 
     dns_id = "".join(
         hosted_zone.get("Id")
-        for hosted_zone in list_hosted_zones()
+        for hosted_zone in route53.list_hosted_zones()
         if hosted_zone.get("Name") == f"{domain_name}."
     )
 
-    return route53.change_resource_record_sets(
+    return route53.client.change_resource_record_sets(
         HostedZoneId=dns_id,
         ChangeBatch={
             "Comment": "",
@@ -47,6 +35,17 @@ def manage_resource_records(
                         "ResourceRecords": [{"Value": f'"{verification_token}"'}],
                     },
                 },
+                {
+                    "Action": action,
+                    "ResourceRecordSet": {
+                        "Name": domain_name,
+                        "Type": "MX",
+                        "TTL": 300,
+                        "ResourceRecords": [
+                            {"Value": "10 inbound-smtp.us-east-1.amazonaws.com"}
+                        ],
+                    },
+                },
             ],
         },
     )
@@ -55,9 +54,7 @@ def manage_resource_records(
 def enable_email_receiving(domain_name: str):
     """Enable receiving emails for a specified domain."""
     # Generate verification token
-    verification_token = ses.verify_domain_identity(Domain=domain_name)[
-        "VerificationToken"
-    ]
+    verification_token = ses.verify_domain_identity_token(domain_name=domain_name)
 
     response = manage_resource_records(
         domain_name=domain_name,
@@ -70,7 +67,7 @@ def enable_email_receiving(domain_name: str):
 
 def disable_email_receiving(domain_name: str):
     """Disable receiving emails for a specified domain."""
-    verification_token = ses.get_identity_verification_attributes(
+    verification_token = ses.client.get_identity_verification_attributes(
         Identities=[domain_name]
     )["VerificationAttributes"][domain_name]["VerificationToken"]
 
@@ -80,4 +77,4 @@ def disable_email_receiving(domain_name: str):
         verification_token=verification_token,
     )
 
-    return ses.delete_identity(Identity=domain_name)
+    return ses.client.delete_identity(Identity=domain_name)
