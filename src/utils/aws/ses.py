6e66beeefdcf5717057462471941263f5d830028
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-07-28 22:57:39 -0500

    add disable receive email logic and refactor

--- src/utils/aws/ses.py
+++ src/utils/aws/ses.py
@@ -18,39 +18,31 @@ def list_hosted_zones(names_only: bool = False):
     return [hosted_zone.get("Name") for hosted_zone in list_hosted_zones()]
 
 
-def get_dns_id(domain_name: str):
-    """Get hosted zone ID."""
+def manage_resource_records(
+    domain_name: str,
+    action: str,
+    verification_token: str,
+    dkim_token_1: str,
+    dkim_token_2: str,
+    dkim_token_3: str,
+):
+    """Manage Route53 Records."""
     if f"{domain_name}." not in list_hosted_zones(names_only=True):
         return "Domain's hosted zone does not exist."
 
-    return "".join(
+    dns_id = "".join(
         hosted_zone.get("Id")
         for hosted_zone in list_hosted_zones()
         if hosted_zone.get("Name") == f"{domain_name}."
     )
 
-
-def enable_email_receiving(domain_name: str):
-    """Enable receiving emails for a specified domain."""
-    dns_id = get_dns_id(domain_name=domain_name)
-
-    # Generate verification token
-    verification_token = ses.verify_domain_identity(Domain=domain_name)[
-        "VerificationToken"
-    ]
-
-    # Generate CNAME record DKIM tokens
-    dkim_token_1, dkim_token_2, dkim_token_3 = ses.verify_domain_dkim(
-        Domain=domain_name
-    )["DkimTokens"]
-
-    response = route53.change_resource_record_sets(
+    return route53.change_resource_record_sets(
         HostedZoneId=dns_id,
         ChangeBatch={
             "Comment": "",
             "Changes": [
                 {
-                    "Action": "UPSERT",
+                    "Action": action,
                     "ResourceRecordSet": {
                         "Name": f"_amazonses.{domain_name}",
                         "Type": "TXT",
@@ -59,7 +51,7 @@ def enable_email_receiving(domain_name: str):
                     },
                 },
                 {
-                    "Action": "UPSERT",
+                    "Action": action,
                     "ResourceRecordSet": {
                         "Name": f"{dkim_token_1}._domainkey.{domain_name}",
                         "Type": "CNAME",
@@ -68,7 +60,7 @@ def enable_email_receiving(domain_name: str):
                     },
                 },
                 {
-                    "Action": "UPSERT",
+                    "Action": action,
                     "ResourceRecordSet": {
                         "Name": f"{dkim_token_2}._domainkey.{domain_name}",
                         "Type": "CNAME",
@@ -77,7 +69,7 @@ def enable_email_receiving(domain_name: str):
                     },
                 },
                 {
-                    "Action": "UPSERT",
+                    "Action": action,
                     "ResourceRecordSet": {
                         "Name": f"{dkim_token_3}._domainkey.{domain_name}",
                         "Type": "CNAME",
@@ -89,10 +81,47 @@ def enable_email_receiving(domain_name: str):
         },
     )
 
+
+def enable_email_receiving(domain_name: str):
+    """Enable receiving emails for a specified domain."""
+    # Generate verification token
+    verification_token = ses.verify_domain_identity(Domain=domain_name)[
+        "VerificationToken"
+    ]
+
+    # Generate CNAME record DKIM tokens
+    dkim_token_1, dkim_token_2, dkim_token_3 = ses.verify_domain_dkim(
+        Domain=domain_name
+    )["DkimTokens"]
+
+    response = manage_resource_records(
+        domain_name=domain_name,
+        action="UPSERT",
+        verification_token=verification_token,
+        dkim_token_1=dkim_token_1,
+        dkim_token_2=dkim_token_2,
+        dkim_token_3=dkim_token_3,
+    )
+
     return response
 
 
 def disable_email_receiving(domain_name: str):
     """Disable receiving emails for a specified domain."""
-    get_dns_id(domain_name=domain_name)
+    verification_token = ses.get_identity_verification_attributes(
+        Identities=[domain_name]
+    )["VerificationAttributes"][domain_name]["VerificationToken"]
+    dkim_token_1, dkim_token_2, dkim_token_3 = ses.get_identity_dkim_attributes(
+        Identities=[domain_name]
+    )["DkimAttributes"][domain_name]["DkimTokens"]
+
+    manage_resource_records(
+        domain_name=domain_name,
+        action="DELETE",
+        verification_token=verification_token,
+        dkim_token_1=dkim_token_1,
+        dkim_token_2=dkim_token_2,
+        dkim_token_3=dkim_token_3,
+    )
+
     return ses.delete_identity(Identity=domain_name)
