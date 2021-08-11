eed8564bbf3c545aec6d15dfb25038c3f26bf2eb
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-08-11 13:49:31 -0500

    fix: receive emails lambda

--- src/lambda_functions/receive_emails.py
+++ src/lambda_functions/receive_emails.py
@@ -24,9 +24,7 @@ def get_email_payload(content):
     message = email.message_from_string(content)
     if message.is_multipart():
         for part in message.walk():
-            if part.get_content_type() == "text/html" and "attachment" not in part.get(
-                "Content-Disposition"
-            ):
+            if part.get_content_type() == "text/html":
                 return part.get_payload(decode=True).decode("ASCII")
     else:
         return message.get_payload(decode=True).decode("ASCII")
