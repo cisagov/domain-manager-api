d09bfcffafbaa13a42db8f6c0a8f3154a89eb8fc
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-09-11 16:03:09 -0500

    add dynamic user notification email to settings

--- src/api/settings.py
+++ src/api/settings.py
@@ -9,6 +9,7 @@ class Settings:
     """Settings."""
 
     SES_FORWARD_EMAIL = None
+    USER_NOTIFICATION_EMAIL = None
 
     def __init__(self) -> None:
         """Init."""
@@ -36,4 +37,7 @@ class Settings:
 
     def to_dict(self):
         """Represent class as dict for responses."""
-        return {"SES_FORWARD_EMAIL": self.SES_FORWARD_EMAIL}
+        return {
+            "SES_FORWARD_EMAIL": self.SES_FORWARD_EMAIL,
+            "USER_NOTIFICATION_EMAIL": self.USER_NOTIFICATION_EMAIL,
+        }
