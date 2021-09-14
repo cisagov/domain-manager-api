ec9a71afd9863963f5fb27a51045ff545f6dea4f
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-09-13 21:22:41 -0500

    test_applications fix

--- tests/views/test_application_views.py
+++ tests/views/test_application_views.py
@@ -8,7 +8,7 @@ from tests.data.application_data import get_applications
 
 def test_applications_get(client, mocker):
     """Test getting list of applications."""
-    mocker.patch("api.manager.ApplicationManager.all", return_value=get_applications(5))
+    mocker.patch("api.manager.ApplicationManager.all", return_value=get_applications(6))
     resp = client.get("/api/applications/")
     data = json.loads(resp.data)
-    assert len(data) == 5
+    assert len(data) == 6
